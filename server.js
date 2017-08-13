73275049a18339af197748213dc002ea29cdc621
saravanra <30778766+saravanra@users.noreply.github.com>
2017-08-13 23:01:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,67 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone = {
+    title: 'Article One | Saravanan Ramasamy',
+    heading: 'Article One',
+    date: 'Aug 13, 2017 | 17:45',
+    content: ` 
+            <p>
+                This is Article One content. I need to write something here. I don't know what to write. Hence I am writing Something. Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.
+            </p>
+            <p>
+                This is Article One content. I need to write something here. I don't know what to write. Hence I am writing Something. Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.
+            </p>
+            <p>
+                This is Article One content. I need to write something here. I don't know what to write. Hence I am writing Something. Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.Something is nothing.
+            </p> `
+};
+
+function createtemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+
+    var htmltemplate = {
+        <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    ;
+    return htmltemplate
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
