8eea1e9fdb3f557047f03befcf4a459b91aa1aad
saravanra <30778766+saravanra@users.noreply.github.com>
2017-08-15 08:46:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ var element = document.getElementById("main-text");
 element.innerHTML = "New value from .js file";
 
 var img = document.getElementById("xtra");
-img.onclick = function () {
-    img.style.marginLeft = "100px";
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
+img.onclick = function () {    
+    var interval = setinterval(moveRight, 100)
+//    img.style.marginLeft = "100px";
 };
