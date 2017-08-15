70240ef96471d58fac942f5445a135fb0c667360
saravanra <30778766+saravanra@users.noreply.github.com>
2017-08-15 08:38:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,9 @@ console.log('Loaded!');
 alert('Hi, this is executed from main.js');
 
 var element = document.getElementById("main-text");
-
 element.innerHTML = "New value from .js file";
+
+var img = document.getElementById("xtra");
+img.onclick = function () {
+    img.style.marginLeft = "100px";
+};
